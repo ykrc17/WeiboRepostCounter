edd31ea901bc2e6d00d31a3bb35147a4ee943689
ykrc17 <ykrc17@163.com>
2016-03-28 23:38:44 +0800

    添加参数：起始页、结束页

--- weiboSpider.py
+++ weiboSpider.py
@@ -1,29 +1,30 @@
-import re, urllib.request, urllib.parse
+import re
+import urllib.parse
+import urllib.request
 
 url = 'http://weibo.cn/'
 
 
 class Spider:
-    def __init__(self, cookie, user_id):
+    def __init__(self, cookie, user_id, start=1, end=10):
         self.cookie = cookie
         self.user_id = user_id
         self.base_url = url + str(user_id) + '?page='
+        self.start = start
+        self.end = end
 
     def run(self):
         f = open('output.csv', 'w')
         f.write('时间,转发,评论,赞\n')
 
-        page_index = 1
-        while True:
+        for page_index in range(self.start, self.end):
+            print(str.format('progress: {:.2}% page: {}', (page_index - self.start) / self.end * 100, page_index))
+
             page = self.send_request_to_weibo(self.base_url + str(page_index))
             post_list = get_post_list(page)
-            if post_list is None:
-                break
-            else:
-                for post in post_list:
-                    post_data = get_data(post)
-                    f.write(str.format('{},{},{},{}\n', get_time(post), post_data[1], post_data[2], post_data[0]))
-                break
+
+            for post in post_list:
+                get_data(f, post)
 
         f.close()
 
@@ -44,12 +45,14 @@ def get_post_list(page):
     return re.findall(pattern, page)
 
 
-def get_data(post):
+def get_data(f, post):
     pattern = r'赞\[(.*?)\].*?转发\[(.*?)\].*?评论\[(.*?)\]'
     data_array = re.findall(pattern, post)
-    return data_array[len(data_array) - 1]
+    if len(data_array) > 0:
+        post_data = data_array[len(data_array) - 1]
+        f.write(str.format('{},{},{},{}\n', get_time(post), post_data[1], post_data[2], post_data[0]))
 
 
 def get_time(post):
-    pattern = r'<span class="ct">(.*?)</span>'
+    pattern = r'<span class="ct">(.*?)&nbsp;.*?</span>'
     return re.search(pattern, post).groups()[0]
